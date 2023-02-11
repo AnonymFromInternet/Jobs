b159b9de839d3510309d37cccd509bafdf0a32e5
arturkeil <kyleartur@gmail.com>
2023-02-11 03:52:47 +0300

    Tmp

--- src/Modules/MainNav/components/MainNav.vue
+++ src/Modules/MainNav/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-main-background-color-1 px-8 mx-auto">
-        <a href="/" class="flex h-full items-center text-xl">Career Agency</a>
+        <a href="/" class="flex h-full items-center text-xl">{{ companyName }}</a>
       </div>
     </div>
   </header>
@@ -11,7 +11,17 @@
 <script>
 // Options API
 export default {
- name: "MainNav",
+  name: "MainNav",
+  data() {
+   return {
+     companyName: "Career Agency",
+   }
+  },
+  methods() {
+    return {
+
+    }
+  },
 }
 </script>
 
